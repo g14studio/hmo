39416203bc14ddbb49c50121cf9daa5064b2474f
G14 Studio <gamaya@g14studio.com>
2016-01-25 00:53:24 -0600

    fix tittle page

--- wp-content/themes/carrental/header.php
+++ wp-content/themes/carrental/header.php
@@ -21,7 +21,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title>HMO El Salvador</title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link href="<?php echo get_template_directory_uri(); ?>/css/lightbox.css" rel="stylesheet" />
